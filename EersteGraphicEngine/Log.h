5d7c311850d066b79aa59f1570e9d03d619143df
fabsgc <fabienbeaudimi@hotmail.fr>
2018-02-17 00:08:00 +0100

    Remove tabs

--- EersteGraphicEngine/Log.h
+++ EersteGraphicEngine/Log.h
@@ -2,24 +2,24 @@
 
 #if defined(EGE_DEBUG)
 #   ifndef EGE_DEBUG_LOG_FILE
-#	define EGE_DEBUG_LOG_FILE "log/debug.log"
-#	endif
+#   define EGE_DEBUG_LOG_FILE "log/debug.log"
+#   endif
 
-#	ifndef EGE_LOG_DEBUG
-#	define EGE_LOG_DEBUG(message)																	\
-		{																							\
-				std::ofstream logFile(EGE_DEBUG_LOG_FILE, std::ios_base::out | std::ios_base::app);	\
-																									\
-				logFile << "Log: " << __FILE__ << ":" << __LINE__;									\
-				logFile << " inside " << __FUNCTION__ << std::endl;									\
-				logFile << "Date: " << __DATE__ << std::endl;										\
-				logFile << "Time: " << __TIME__ << std::endl;										\
-				logFile << "Message: " << message << std::endl;										\
-				logFile << "############################################: " << std::endl;			\
-		}																					
-#	endif
+#   ifndef EGE_LOG_DEBUG
+#   define EGE_LOG_DEBUG(message)                                                                   \
+        {                                                                                           \
+                std::ofstream logFile(EGE_DEBUG_LOG_FILE, std::ios_base::out | std::ios_base::app); \
+                                                                                                    \
+                logFile << "Log: " << __FILE__ << ":" << __LINE__;                                  \
+                logFile << " inside " << __FUNCTION__ << std::endl;                                 \
+                logFile << "Date: " << __DATE__ << std::endl;                                       \
+                logFile << "Time: " << __TIME__ << std::endl;                                       \
+                logFile << "Message: " << message << std::endl;                                     \
+                logFile << "############################################: " << std::endl;           \
+        }                                                                                   
+#   endif
 #else 
-#	ifndef EGE_DEBUG
-#	define EGE_DEBUG(message)
-#	endif
+#   ifndef EGE_DEBUG
+#   define EGE_DEBUG(message)
+#   endif
 #endif
--- EersteGraphicEngine/Timer.h
+++ EersteGraphicEngine/Timer.h
@@ -18,4 +18,3 @@ namespace ee
         std::chrono::time_point<std::chrono::high_resolution_clock> _startTime;
     };
 }
-
