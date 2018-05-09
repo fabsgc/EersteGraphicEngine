ad79bd1cea18d11ac297e651c47672f209ffbe52
fabsgc <fabienbeaudimi@hotmail.fr>
2018-05-09 16:48:16 +0200

    Add Shader compilation

--- EersteGraphicEngine/bin64/Data/Shader/Color/Color_PS.hlsl
+++ EersteGraphicEngine/bin64/Data/Shader/Color/Color_PS.hlsl
@@ -9,8 +9,8 @@ SamplerState ColorSampler : register(s0);
 
 struct PS_INPUT
 {
-    float4 Position             : SV_POSITION;
-    float4 Color                : COLOR0;
+    float4 Position : SV_POSITION;
+    float4 Color    : COLOR0;
 };
 
 float4 PS_MAIN( PS_INPUT IN ) : SV_Target
--- EersteGraphicEngine/bin64/Data/Shader/Color/Color_VS.hlsl
+++ EersteGraphicEngine/bin64/Data/Shader/Color/Color_VS.hlsl
@@ -9,14 +9,14 @@ SamplerState ColorSampler : register(s0);
 
 struct VS_INPUT
 {
-    float4 Position             : POSITION;
-    float4 Color                : COLOR0;
+    float4 Position : POSITION;
+    float4 Color    : COLOR0;
 };
 
 struct VS_OUTPUT
 {
-    float4 Position             : SV_POSITION;
-    float4 Color                : COLOR0;
+    float4 Position : SV_POSITION;
+    float4 Color    : COLOR0;
 };
 
 VS_OUTPUT VS_MAIN( VS_INPUT IN )
