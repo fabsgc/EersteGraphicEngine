7c7380b98aa6daed0bf0ff27f0f46064dd0a3cb3
fabsgc <fabienbeaudimi@hotmail.fr>
2018-05-25 16:12:35 +0200

    Remove tabs from files

--- EersteGraphicEngine/bin64/Data/Shader/Color/Color_PS.hlsl
+++ EersteGraphicEngine/bin64/Data/Shader/Color/Color_PS.hlsl
@@ -13,11 +13,11 @@ struct PS_INPUT
     float4 Color    : COLOR0;
     float2 Texture  : TEXCOORD0;
     float3 Normal   : NORMAL;
-	float3 Tangent  : TANGENT;
-	float3 Binormal : BINORMAL;
+    float3 Tangent  : TANGENT;
+    float3 Binormal : BINORMAL;
 };
 
 float4 PS_MAIN( PS_INPUT IN ) : SV_Target
 {
-	return IN.Color;
+    return IN.Color;
 }
--- EersteGraphicEngine/bin64/Data/Shader/Color/Color_VS.hlsl
+++ EersteGraphicEngine/bin64/Data/Shader/Color/Color_VS.hlsl
@@ -13,8 +13,8 @@ struct VS_INPUT
     float4 Color    : COLOR0;
     float2 Texture  : TEXCOORD0;
     float3 Normal   : NORMAL;
-	float3 Tangent  : TANGENT;
-	float3 Binormal : BINORMAL;
+    float3 Tangent  : TANGENT;
+    float3 Binormal : BINORMAL;
 };
 
 struct VS_OUTPUT
@@ -23,20 +23,20 @@ struct VS_OUTPUT
     float4 Color    : COLOR0;
     float2 Texture  : TEXCOORD0;
     float3 Normal   : NORMAL;
-	float3 Tangent  : TANGENT;
-	float3 Binormal : BINORMAL;
+    float3 Tangent  : TANGENT;
+    float3 Binormal : BINORMAL;
 };
 
 VS_OUTPUT VS_MAIN( VS_INPUT IN )
 {
-	VS_OUTPUT output = (VS_OUTPUT)0;
-	
+    VS_OUTPUT output = (VS_OUTPUT)0;
+    
     output.Position = mul( IN.Position, World );
     output.Position = mul( output.Position, View );
     output.Position = mul( output.Position, Projection );
     output.Color    = IN.Color;
     output.Normal   = normalize(mul(float4(IN.Normal, 0.0f), World)).xyz;
-	output.Tangent  = normalize(mul(float4(IN.Tangent, 0.0f), World)).xyz;
+    output.Tangent  = normalize(mul(float4(IN.Tangent, 0.0f), World)).xyz;
     output.Binormal = cross(output.Normal, output.Tangent);
 
     return output;
