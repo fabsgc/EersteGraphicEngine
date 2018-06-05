b28560bb879b691731fa8d0c4c31f00f0223dccb
fabsgc <fabienbeaudimi@hotmail.fr>
2018-06-05 22:10:55 +0200

    Keep all three solutions up to date

--- EersteGraphicEngine/MaterialManager.cpp
+++ EersteGraphicEngine/MaterialManager.cpp
@@ -13,6 +13,17 @@ namespace ege
 
     void MaterialManager::Initialise()
     {
+#if defined(EGE_MATERIAL_FOLDER) && defined(EGE_CONFIG_MATERIAL_FILE)
+        tinyxml2::XMLDocument document;
+        document.LoadFile(EGE_CONFIG_MATERIAL_FILE);
+
+        tinyxml2::XMLElement* materialsElement = document.FirstChildElement("materials");
+        EGE_ASSERT_ERROR((materialsElement != nullptr), "Material file malformed");
+
+        for (tinyxml2::XMLElement* materialElement = materialsElement->FirstChildElement("material"); materialElement != nullptr; materialElement = materialElement->NextSiblingElement())
+        {
+        }
+#endif
     }
 
     MaterialManager& gMaterialManager()
